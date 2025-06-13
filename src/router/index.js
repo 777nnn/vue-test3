81ae944378d66c5c1520daa973f6ccc70a8c891e
Nanako Takahashi <nanako.takahashi@sato-global.com>
2025-06-13 13:44:51 +0900

    [feat] ヘッダーコンポーネント追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'  // 今のパスに合わせてください
 
-const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID'
-};
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  // 他のページのルートも追加してください（例: /add, /page1など）
+]
 
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
-const app = createApp(App);
-app.config.globalProperties.$db = db;
-app.use(router);
-app.mount('#app');
+export default router
